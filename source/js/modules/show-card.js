4f2fcbc84c8ab26ab77000e978ce5062c7b3edde
Kate <skoropeya@inbox.ru>
2022-03-19 20:39:11 +0300

    исправления

--- source/js/modules/show-card.js
+++ source/js/modules/show-card.js
@@ -5,15 +5,18 @@ const mediaQuery = window.matchMedia('(pointer:coarse)');
 const onTouchCard = (evt) => {
   evt.preventDefault();
 
-  if (evt.target.matches('.card')) {
+  if (evt.target.matches('.card__overlay') || evt.target.matches('.card__name')) {
     cards.forEach((card) => {
-      if (evt.target === card) {
-        evt.target.dataset.isTouch = true;
+      if (evt.target.closest('.card') === card) {
+        card.dataset.isTouch = 'true';
       } else {
-        card.dataset.isTouch = false;
+        card.dataset.isTouch = 'false';
       }
-      card.dataset.isTouch = false;
     });
+  }
+
+  if (evt.target.matches('.card__link-booking')) {
+    document.location.replace(evt.target.href);
   }
 };
 
